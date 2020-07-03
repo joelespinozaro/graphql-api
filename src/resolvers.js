21da7f440cdea6b8e03bbf28fcfa8a1a179baf56
joelespinozaro <joelespinozaro@gmail.com>
2020-07-03 10:43:49 -0500

    Creación de modelo user e import en resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,23 +1,25 @@
 import { tasks } from "./sample";
 
+import User from "./models/User";
+
 export const resolvers = {
-    Query: {
-        hello: () => {
-            return "Hello World with Graphql";
-        },
-        greet(root, { name }, ctx) {
-            console.log(ctx);
-            return `Hola ${name}`;
-        },
-        tasks() {
-            return tasks;
-        },
+  Query: {
+    hello: () => {
+      return "Hello World with Graphql";
     },
-    Mutation: {
-        createTask(_, { input }) {
-            input._id = tasks.length;
-            tasks.push(input);
-            return input;
-        },
+    greet(root, { name }, ctx) {
+      console.log(ctx);
+      return `Hola ${name}`;
     },
+    tasks() {
+      return tasks;
+    },
+  },
+  Mutation: {
+    createTask(_, { input }) {
+      input._id = tasks.length;
+      tasks.push(input);
+      return input;
+    },
+  },
 };
